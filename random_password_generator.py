435781a726153d82b1b8d4a8c0bb1b55e88ee748
Pallabi Panja <dreamerpallabi01@gmail.com>
2026-06-13 02:46:06 +0530

    Comments are added

--- random_password_generator.py
+++ random_password_generator.py
@@ -5,29 +5,33 @@ It should contain atleast one of each character type.
 import random
 import string
 
-def password_generator():
-    while True:
-        try:
+def password_generator():             #defining a function called password_generator
+    while True:                       #creating a loop for user so that they cannot give any wrong value
+        try:                          #used error handeling to get the right value
             length = int(input("Enter the password length: ").strip())
             break
         except ValueError:
             print("Enter a valid number...")
 
     
-    # if length < 5:
+    # if length < 5:                      #using a condition so that the password length given by the user must be greater than 5
     #     print("password length must be contain atleast 5 charaters.")
-    #     return
-        
-    include_uppercase = input("Do you wanna include uppercase letters? (y/n): ").lower().strip()
+    #     return                          #i don't know what kind of mistake i'm doing here cause this i not getting executed, no matter what :(
+
+
+    # Getting user's preferences   
+    include_uppercase = input("Do you wanna include uppercase letters? (y/n): ").lower().strip()  
     include_lowercase = input("Do you wanna include lowercase letters? (y/n): ").lower().strip()
     include_digits = input("Do you wanna include digits? (y/n): ").lower().strip()
     include_special = input("Do you wanna include special characters? (y/n): ").lower().strip()
 
+
+# Here python is using it's random module to give accurate passwors using user's preffered characters
     lower = string.ascii_lowercase
     upper = string.ascii_uppercase if include_uppercase == "y" else ""
     digits = string.digits if include_digits == "y" else ""
     special = string.punctuation if include_special == "y" else ""
-    all_characters = upper + lower + digits + special
+    all_characters = upper + lower + digits + special       # This is the variable that will be used at last as a final password
 
     required_charecters = []
     if include_uppercase == "y":
@@ -39,16 +43,16 @@ def password_generator():
     if include_special == "y":
         required_charecters.append(random.choice(special))
 
-    remaining_length = length - len(required_charecters)
+    remaining_length = length - len(required_charecters)    # Let's assume the user choosed the password length 10, so in that case this variable will give the remaining length after appending all must-used characters
     password = required_charecters
 
-    for idx in range(remaining_length):
+    for idx in range(remaining_length):                   
         character = random.choice(all_characters)
         password.append(character)
 
-    random.shuffle(password)
+    random.shuffle(password)                                # as the function is putting all the nessesary characters 1st and then adding random characters on it's choice, there should be no biased password, that's why it's using suffle
 
-    str_password = "".join(password)
+    str_password = "".join(password)                        # This is converting the list of characters to a string
     
-password = password_generator()
+password = password_generator()                             # calling the function.
 print(password)
